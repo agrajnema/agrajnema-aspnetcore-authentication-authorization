59d6585c94f0ac8408751556d27d3c0e8854408e
agraj nema <agraj.nema@tal.com.au>
2020-05-27 14:48:21 +1000

    Add project files.

---
+++ AuthenticationAuthorization/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AuthenticationAuthorization.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthenticationAuthorization.Areas.Identity.IdentityHostingStartup))]
+namespace AuthenticationAuthorization.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
